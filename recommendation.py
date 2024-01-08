5413ff0dd6a8dc213172bcee7a85e33a9a4e5810
PrajwalRaj21 <christprajwal@gmail.com>
2024-01-08 10:38:23 +0545

    Completed

--- recommendation.py
+++ recommendation.py
@@ -1,216 +1,139 @@
 import numpy as np
 import pandas as pd
 
-#Class for Popularity based Recommender System model
-class popularity_recommender_py():
+class popularity_recommender_car_rental():
     def __init__(self):
         self.train_data = None
         self.user_id = None
-        self.item_id = None
+        self.car_id = None
         self.popularity_recommendations = None
         
-    #Create the popularity based recommender system model
-    def create(self, train_data, user_id, item_id):
+    def create(self, train_data, user_id, car_id):
         self.train_data = train_data
         self.user_id = user_id
-        self.item_id = item_id
+        self.car_id = car_id
 
-        #Get a count of user_ids for each unique song as recommendation score
-        train_data_grouped = train_data.groupby([self.item_id]).agg({self.user_id: 'count'}).reset_index()
-        train_data_grouped.rename(columns = {'user_id': 'score'},inplace=True)
-    
-        #Sort the songs based upon recommendation score
-        train_data_sort = train_data_grouped.sort_values(['score', self.item_id], ascending = [0,1])
-    
-        #Generate a recommendation rank based upon score
+        train_data_grouped = train_data.groupby([self.car_id]).agg({self.user_id: 'count'}).reset_index()
+        train_data_grouped.rename(columns={'user_id': 'score'}, inplace=True)
+
+        train_data_sort = train_data_grouped.sort_values(['score', self.car_id], ascending=[0, 1])
         train_data_sort['Rank'] = train_data_sort['score'].rank(ascending=0, method='first')
-        
-        #Get the top 10 recommendations
+
         self.popularity_recommendations = train_data_sort.head(10)
 
-    #Use the popularity based recommender system model to
-    #make recommendations
-    def recommend(self, user_id):    
-        user_recommendations = self.popularity_recommendations
-        
-        #Add user_id column for which the recommendations are being generated
-        user_recommendations['user_id'] = user_id
-    
-        #Bring user_id column to the front
-        cols = user_recommendations.columns.tolist()
-        cols = cols[-1:] + cols[:-1]
-        user_recommendations = user_recommendations[cols]
-        
-        return user_recommendations
-    
+    def recommend(self, user_id):
+        car_recommendations = self.popularity_recommendations
+        car_recommendations['user_id'] = user_id
 
-#Class for Item similarity based Recommender System model
-class item_similarity_recommender_py():
+        cols = car_recommendations.columns.tolist()
+        cols = cols[-1:] + cols[:-1]
+        car_recommendations = car_recommendations[cols]
+
+        return car_recommendations
+
+
+class item_similarity_recommender_car_rental():
     def __init__(self):
         self.train_data = None
         self.user_id = None
-        self.item_id = None
-        self.cooccurence_matrix = None
-        self.songs_dict = None
-        self.rev_songs_dict = None
+        self.car_id = None
+        self.cooccurrence_matrix = None
+        self.cars_dict = None
+        self.rev_cars_dict = None
         self.item_similarity_recommendations = None
-        
-    #Get unique items (songs) corresponding to a given user
-    def get_user_items(self, user):
-        user_data = self.train_data[self.train_data[self.user_id] == user]
-        user_items = list(user_data[self.item_id].unique())
-        
-        return user_items
-        
-    #Get unique users for a given item (song)
-    def get_item_users(self, item):
-        item_data = self.train_data[self.train_data[self.item_id] == item]
-        item_users = set(item_data[self.user_id].unique())
-            
-        return item_users
-        
-    #Get unique items (songs) in the training data
-    def get_all_items_train_data(self):
-        all_items = list(self.train_data[self.item_id].unique())
-            
-        return all_items
-        
-    #Construct cooccurence matrix
-    def construct_cooccurence_matrix(self, user_songs, all_songs):
-            
-        ####################################
-        #Get users for all songs in user_songs.
-        ####################################
-        user_songs_users = []        
-        for i in range(0, len(user_songs)):
-            user_songs_users.append(self.get_item_users(user_songs[i]))
-            
-        ###############################################
-        #Initialize the item cooccurence matrix of size 
-        #len(user_songs) X len(songs)
-        ###############################################
-        cooccurence_matrix = np.matrix(np.zeros(shape=(len(user_songs), len(all_songs))), float)
-           
-        #############################################################
-        #Calculate similarity between user songs and all unique songs
-        #in the training data
-        #############################################################
-        for i in range(0,len(all_songs)):
-            #Calculate unique listeners (users) of song (item) i
-            songs_i_data = self.train_data[self.train_data[self.item_id] == all_songs[i]]
-            users_i = set(songs_i_data[self.user_id].unique())
-            
-            for j in range(0,len(user_songs)):       
-                    
-                #Get unique listeners (users) of song (item) j
-                users_j = user_songs_users[j]
-                    
-                #Calculate intersection of listeners of songs i and j
-                users_intersection = users_i.intersection(users_j)
-                
-                #Calculate cooccurence_matrix[i,j] as Jaccard Index
-                if len(users_intersection) != 0:
-                    #Calculate union of listeners of songs i and j
-                    users_union = users_i.union(users_j)
-                    
-                    cooccurence_matrix[j,i] = float(len(users_intersection))/float(len(users_union))
-                else:
-                    cooccurence_matrix[j,i] = 0
-                    
-        
-        return cooccurence_matrix
 
-    
-    #Use the cooccurence matrix to make top recommendations
-    def generate_top_recommendations(self, user, cooccurence_matrix, all_songs, user_songs):
-        print("Non zero values in cooccurence_matrix :%d" % np.count_nonzero(cooccurence_matrix))
-        
-        #Calculate a weighted average of the scores in cooccurence matrix for all user songs.
-        user_sim_scores = cooccurence_matrix.sum(axis=0)/float(cooccurence_matrix.shape[0])
+    def get_user_cars(self, user):
+        user_data = self.train_data[self.train_data[self.user_id] == user]
+        user_cars = list(user_data[self.car_id].unique())
+
+        return user_cars
+
+    def get_car_users(self, car):
+        car_data = self.train_data[self.train_data[self.car_id] == car]
+        car_users = set(car_data[self.user_id].unique())
+
+        return car_users
+
+    def get_all_cars_train_data(self):
+        all_cars = list(self.train_data[self.car_id].unique())
+
+        return all_cars
+
+    def construct_cooccurrence_matrix(self, user_cars, all_cars):
+        user_cars_users = []
+        for i in range(0, len(user_cars)):
+            user_cars_users.append(self.get_car_users(user_cars[i]))
+
+        cooccurrence_matrix = np.matrix(np.zeros(shape=(len(user_cars), len(all_cars))), float)
+
+        for i in range(0, len(all_cars)):
+            cars_i_data = self.train_data[self.train_data[self.car_id] == all_cars[i]]
+            users_i = set(cars_i_data[self.user_id].unique())
+
+            for j in range(0, len(user_cars)):
+                users_j = user_cars_users[j]
+                users_intersection = users_i.intersection(users_j)
+
+                if len(users_intersection) != 0:
+                    users_union = users_i.union(users_j)
+                    cooccurrence_matrix[j, i] = float(len(users_intersection)) / float(len(users_union))
+                else:
+                    cooccurrence_matrix[j, i] = 0
+
+        return cooccurrence_matrix
+
+    def generate_top_recommendations(self, user, cooccurrence_matrix, all_cars, user_cars):
+        print("Non zero values in cooccurrence_matrix :%d" % np.count_nonzero(cooccurrence_matrix))
+
+        user_sim_scores = cooccurrence_matrix.sum(axis=0) / float(cooccurrence_matrix.shape[0])
         user_sim_scores = np.array(user_sim_scores)[0].tolist()
- 
-        #Sort the indices of user_sim_scores based upon their value
-        #Also maintain the corresponding score
-        sort_index = sorted(((e,i) for i,e in enumerate(list(user_sim_scores))), reverse=True)
-    
-        #Create a dataframe from the following
-        columns = ['user_id', 'song', 'score', 'rank']
-        #index = np.arange(1) # array of numbers for the number of samples
-        df = pandas.DataFrame(columns=columns)
-         
-        #Fill the dataframe with top 10 item based recommendations
-        rank = 1 
-        for i in range(0,len(sort_index)):
-            if ~np.isnan(sort_index[i][0]) and all_songs[sort_index[i][1]] not in user_songs and rank <= 10:
-                df.loc[len(df)]=[user,all_songs[sort_index[i][1]],sort_index[i][0],rank]
-                rank = rank+1
-        
-        #Handle the case where there are no recommendations
+
+        sort_index = sorted(((e, i) for i, e in enumerate(list(user_sim_scores))), reverse=True)
+
+        columns = ['user_id', 'car', 'score', 'rank']
+        df = pd.DataFrame(columns=columns)
+
+        rank = 1
+        for i in range(0, len(sort_index)):
+            if ~np.isnan(sort_index[i][0]) and all_cars[sort_index[i][1]] not in user_cars and rank <= 10:
+                df.loc[len(df)] = [user, all_cars[sort_index[i][1]], sort_index[i][0], rank]
+                rank = rank + 1
+
         if df.shape[0] == 0:
-            print("The current user has no songs for training the item similarity based recommendation model.")
+            print("The current user has no cars for training the item similarity based recommendation model.")
             return -1
         else:
             return df
- 
-    #Create the item similarity based recommender system model
-    def create(self, train_data, user_id, item_id):
+
+    def create(self, train_data, user_id, car_id):
         self.train_data = train_data
         self.user_id = user_id
-        self.item_id = item_id
+        self.car_id = car_id
 
-    #Use the item similarity based recommender system model to
-    #make recommendations
     def recommend(self, user):
-        
-        ########################################
-        #A. Get all unique songs for this user
-        ########################################
-        user_songs = self.get_user_items(user)    
-            
-        print("No. of unique songs for the user: %d" % len(user_songs))
-        
-        ######################################################
-        #B. Get all unique items (songs) in the training data
-        ######################################################
-        all_songs = self.get_all_items_train_data()
-        
-        print("no. of unique songs in the training set: %d" % len(all_songs))
-         
-        ###############################################
-        #C. Construct item cooccurence matrix of size 
-        #len(user_songs) X len(songs)
-        ###############################################
-        cooccurence_matrix = self.construct_cooccurence_matrix(user_songs, all_songs)
-        
-        #######################################################
-        #D. Use the cooccurence matrix to make recommendations
-        #######################################################
-        df_recommendations = self.generate_top_recommendations(user, cooccurence_matrix, all_songs, user_songs)
-                
+        user_cars = self.get_user_cars(user)
+
+        print("No. of unique cars for the user: %d" % len(user_cars))
+
+        all_cars = self.get_all_cars_train_data()
+
+        print("No. of unique cars in the training set: %d" % len(all_cars))
+
+        cooccurrence_matrix = self.construct_cooccurrence_matrix(user_cars, all_cars)
+
+        df_recommendations = self.generate_top_recommendations(user, cooccurrence_matrix, all_cars, user_cars)
+
         return df_recommendations
-    
-    #Get similar items to given items
-    def get_similar_items(self, item_list):
-        
-        user_songs = item_list
-        
-        ######################################################
-        #B. Get all unique items (songs) in the training data
-        ######################################################
-        all_songs = self.get_all_items_train_data()
-        
-        print("no. of unique songs in the training set: %d" % len(all_songs))
-         
-        ###############################################
-        #C. Construct item cooccurence matrix of size 
-        #len(user_songs) X len(songs)
-        ###############################################
-        cooccurence_matrix = self.construct_cooccurence_matrix(user_songs, all_songs)
-        
-        #######################################################
-        #D. Use the cooccurence matrix to make recommendations
-        #######################################################
+
+    def get_similar_cars(self, car_list):
+        user_cars = car_list
+        all_cars = self.get_all_cars_train_data()
+
+        print("No. of unique cars in the training set: %d" % len(all_cars))
+
+        cooccurrence_matrix = self.construct_cooccurrence_matrix(user_cars, all_cars)
+
         user = ""
-        df_recommendations = self.generate_top_recommendations(user, cooccurence_matrix, all_songs, user_songs)
-         
+        df_recommendations = self.generate_top_recommendations(user, cooccurrence_matrix, all_cars, user_cars)
+
         return df_recommendations
